4eab5c0f637d9763f567b4b5357a3bf8201f2e4b
Leezed <leezed525@qq.com>
2022-08-12 15:25:56 +0800

    创建双端链表，线性表文件

---
+++ 算法学习/考研数据结构/线性表/OrderList.cpp
@@ -0,0 +1,61 @@
+//
+// Created by Leezed on 2022/7/19.
+// 顺序表
+//
+#include<bits/stdc++.h>
+
+using namespace std;
+const int maxSize = 20 + 5;
+
+
+typedef struct {
+    int data[maxSize];
+    int length;
+} OList;
+
+
+void printList(OList list) {
+    for (int i = 0; i < list.length; i += 1) {
+        printf("%d ", list.data[i]);
+    }
+    printf("\n");
+}
+
+void clearOrderList(OList &list) {
+    memset(list.data, 0, sizeof(list.data));
+    list.length = 0;
+}
+
+bool initOrderList(OList &list, int data[],int len) {
+    if (len > maxSize) {
+        return false;
+    }
+    for (int i = 0; i < len; i += 1) {
+        list.data[i] = data[i];
+    }
+    list.length = len;
+}
+
+
+bool insertOrderList(OList &list, int order, int data) {
+    if (order > list.length + 1 || order <= 0) {
+        return false;
+    }
+    if (list.length == maxSize) {
+        return false;
+    }
+
+}
+
+
+int main() {
+
+    OList list;
+    clearOrderList(list);
+
+    int initData[] = {7, 2, 5, 4, 5};
+    initOrderList(list, initData,5);
+    printList(list);
+
+    return 0;
+}
---
+++ 算法学习/考研数据结构/线性表/SingleLinkedListWithoutHead.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Leezed on 2022/7/19.
+//
+#include<bits/stdc++.h>
+using namespace  std;
+
+int main (){
+    cout << "你好世界23" << endl;
+    return 0;
+}
