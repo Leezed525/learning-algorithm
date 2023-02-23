0a88ced4209997fbb848a993b1c04c31ce14bb15
Leezed <leezed525@qq.com>
2023-02-23 17:58:23 +0800

    first Complete

---
+++ zjutOj/1001.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    while (n--)
+    {
+        int a, b;
+        char ch;
+        cin >> a >> ch >> b;
+        // cout << a << ch << b << endl;
+        if (ch == '+')
+        {
+            cout << a + b << endl;
+        }
+        else if (ch == '-')
+        {
+            cout << a - b << endl;
+        }
+        else if (ch == '*')
+        {
+            cout << a * b << endl;
+        }
+    }
+
+    // system("pause");
+    return 0;
+}
