6e3ccd12b93d1a7c22be0bd9a807cdf4dd8f08e5
Leezed525 <leezed525@qq.com>
2023-09-19 22:20:22 +0800

    导入了以前写的题目

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,11 +1,32 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <stdio.h>
+
+int gcd(int a, int b){
+    return b == 0 ? a : gcd(b, a % b);
+}
 
 int main() {
-    int a;
-    cin >> a;
-    cout << a;
-    cout << "hello worlä½ å¥½123456" << endl;
-    // system("pause");
+//    float c, f;
+//    scanf("%f", &c);
+//    f = c * 1.8 + 32;
+//    printf("%.2f\n", f);
+//    return 0;
+//    int x, y, r, m;
+//    printf("x = %d, y = %d\n", x, y);
+//    printf("r = %d, m = %d\n", r, m);
+//    scanf("%d%d", &x, &y);
+//    if (x < y)
+//        m = y;
+//    y = x;
+//    printf("init m = %d\n", m);
+//    x = m;
+//
+//    printf("init r = %d\n", r);
+//    while (r != 0) {
+//        r = x % y;
+//        x = y;
+//        y = r;
+//    }
+//    printf("%d\n", x);
+    printf("ÄãºÃÊÀ½ç\n");
     return 0;
 }
