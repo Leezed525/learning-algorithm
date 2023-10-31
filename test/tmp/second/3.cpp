a689d267d39b9d327e58bcb95c6fa75baca076be
Leezed525 <leezed525@qq.com>
2023-10-31 16:28:17 +0800

    修改一些代码

--- test/tmp/second/3.cpp
+++ test/tmp/second/3.cpp
@@ -8,8 +8,14 @@ int main() {
     scanf("%d", &x);
     if (x % 3 == 0)
         printf("该数%d能被3整除\n", x);
+    else
+        printf("该数%d不能被3整除\n", x);
     if (x % 5 == 0)
         printf("该数%d能被5整除\n", x);
+    else
+        printf("该数%d不能被5整除\n", x);
     if (x % 7 == 0)
         printf("该数%d能被7整除\n", x);
+    else
+        printf("该数%d不能被7整除\n", x);
 }
--- test/tmp/second/8.cpp
+++ test/tmp/second/8.cpp
@@ -2,10 +2,13 @@
 // Created by Leezed on 2023/10/30.
 //
 #include <stdio.h>
-int main(){
+
+int main() {
     setbuf(stdout, NULL);//关闭缓冲区  这段代码可以不用管
     int x;
-    while(scanf("%d",&x)!=EOF) {//scanf() != EOF的意思是输入到文件结束
+    int count = 0;
+    while (scanf("%d", &x) != EOF && count <= 50) {//scanf() != EOF的意思是输入到文件结束
         printf("%d\n", x * x);
+        count += 1;
     }
 }
