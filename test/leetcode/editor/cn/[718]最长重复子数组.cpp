541cce92b59bbbf41a1cb2b947468a9e1a63470c
Leezed525 <leezed525@qq.com>
2025-03-12 16:32:40 +0800

    完成5道Dp

---
+++ test/leetcode/editor/cn/[718]最长重复子数组.cpp
@@ -0,0 +1,54 @@
+//给两个整数数组 nums1 和 nums2 ，返回 两个数组中 公共的 、长度最长的子数组的长度 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums1 = [1,2,3,2,1], nums2 = [3,2,1,4,7]
+//输出：3
+//解释：长度最长的公共子数组是 [3,2,1] 。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums1 = [0,0,0,0,0], nums2 = [0,0,0,0,0]
+//输出：5
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums1.length, nums2.length <= 1000 
+// 0 <= nums1[i], nums2[i] <= 100 
+// 
+//
+// Related Topics 数组 二分查找 动态规划 滑动窗口 哈希函数 滚动哈希 👍 1158 👎 0
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        int dp[1001][1001] = {0};
+
+        int res = 0;
+
+        for (int i = 1; i <= nums1.size(); i += 1) {
+            for (int j = 1; j <= nums2.size(); j += 1) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                res = max(res, dp[i][j]);
+            }
+        }
+
+        return res;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
