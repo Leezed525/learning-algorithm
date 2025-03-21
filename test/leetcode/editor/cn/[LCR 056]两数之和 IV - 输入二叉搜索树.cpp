5eb7216f1bbda4e2995bbc8cfd62292d8382f005
Leezed525 <leezed525@qq.com>
2025-03-21 18:05:11 +0800

    完成2题

---
+++ test/leetcode/editor/cn/[LCR 056]两数之和 IV - 输入二叉搜索树.cpp
@@ -0,0 +1,68 @@
+//给定一个二叉搜索树的 根节点 root 和一个整数 k , 请判断该二叉搜索树中是否存在两个节点它们的值之和等于 k 。假设二叉搜索树中节点的值均唯一。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入: root = [8,6,10,5,7,9,11], k = 12
+//输出: true
+//解释: 节点 5 和节点 7 之和等于 12
+// 
+//
+// 示例 2： 
+//
+// 
+//输入: root = [8,6,10,5,7,9,11], k = 22
+//输出: false
+//解释: 不存在两个节点值之和为 22 的节点
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 二叉树的节点个数的范围是 [1, 10⁴]. 
+// -10⁴ <= Node.val <= 10⁴ 
+// root 为二叉搜索树 
+// -10⁵ <= k <= 10⁵ 
+// 
+//
+// 
+//
+// 注意：本题与主站 653 题相同： https://leetcode-cn.com/problems/two-sum-iv-input-is-a-bst/
+// 
+//
+// Related Topics 数组 滑动窗口 👍 71 👎 0
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<int, int> mp;
+
+    bool findTarget(TreeNode *root, int k) {
+        if (root == nullptr) return false;
+        if (findTarget(root->left, k)) return true;
+        if (mp[k - root->val] == 1) return true;
+        mp[root->val] = 1;
+        if (findTarget(root->right, k)) return true;
+        return false;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
