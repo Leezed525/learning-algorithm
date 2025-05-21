adc49de7c79e841d344c916dbf92e9de5156c251
Leezed525 <leezed525@qq.com>
2025-05-21 17:31:56 +0800

    完成2题

---
+++ test/leetcode/editor/cn/[207]课程表.cpp
@@ -0,0 +1,80 @@
+//你这个学期必须选修 numCourses 门课程，记为 0 到 numCourses - 1 。 
+//
+// 在选修某些课程之前需要一些先修课程。 先修课程按数组 prerequisites 给出，其中 prerequisites[i] = [ai, bi] ，表
+//示如果要学习课程 ai 则 必须 先学习课程 bi 。 
+//
+// 
+// 例如，先修课程对 [0, 1] 表示：想要学习课程 0 ，你需要先完成课程 1 。 
+// 
+//
+// 请你判断是否可能完成所有课程的学习？如果可以，返回 true ；否则，返回 false 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：numCourses = 2, prerequisites = [[1,0]]
+//输出：true
+//解释：总共有 2 门课程。学习课程 1 之前，你需要完成课程 0 。这是可能的。 
+//
+// 示例 2： 
+//
+// 
+//输入：numCourses = 2, prerequisites = [[1,0],[0,1]]
+//输出：false
+//解释：总共有 2 门课程。学习课程 1 之前，你需要先完成​课程 0 ；并且学习课程 0 之前，你还应先完成课程 1 。这是不可能的。 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= numCourses <= 2000 
+// 0 <= prerequisites.length <= 5000 
+// prerequisites[i].length == 2 
+// 0 <= ai, bi < numCourses 
+// prerequisites[i] 中的所有课程对 互不相同 
+// 
+//
+// Related Topics 深度优先搜索 广度优先搜索 图 拓扑排序 👍 2149 👎 0
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>> &prerequisites) {
+        int maxn = 2e3 + 5;
+        vector<int> in(maxn, 0);
+        vector<vector<int>> g = vector<vector<int>>(numCourses, vector<int>());
+
+        for (auto &p: prerequisites) {
+            g[p[1]].push_back(p[0]);
+            in[p[0]]++;
+        }
+
+        queue<int> q;
+
+        for (int i = 0; i < numCourses; i += 1) {
+            if (in[i] == 0) q.push(i);
+        }
+
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+            numCourses -= 1;
+            for (auto &v: g[u]) {
+                in[v] -= 1;
+                if (in[v] == 0) {
+                    q.push(v);
+                }
+            }
+        }
+
+        return numCourses == 0;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ test/leetcode/editor/cn/[208]实现 Trie (前缀树).cpp
@@ -0,0 +1,122 @@
+//Trie（发音类似 "try"）或者说 前缀树 是一种树形数据结构，用于高效地存储和检索字符串数据集中的键。这一数据结构有相当多的应用情景，例如自动补全和拼
+//写检查。 
+//
+// 请你实现 Trie 类： 
+//
+// 
+// Trie() 初始化前缀树对象。 
+// void insert(String word) 向前缀树中插入字符串 word 。 
+// boolean search(String word) 如果字符串 word 在前缀树中，返回 true（即，在检索之前已经插入）；否则，返回 
+//false 。 
+// boolean startsWith(String prefix) 如果之前已经插入的字符串 word 的前缀之一为 prefix ，返回 true ；否
+//则，返回 false 。 
+// 
+//
+// 
+//
+// 示例： 
+//
+// 
+//输入
+//["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+//[[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+//输出
+//[null, null, true, false, true, null, true]
+//
+//解释
+//Trie trie = new Trie();
+//trie.insert("apple");
+//trie.search("apple");   // 返回 True
+//trie.search("app");     // 返回 False
+//trie.startsWith("app"); // 返回 True
+//trie.insert("app");
+//trie.search("app");     // 返回 True
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= word.length, prefix.length <= 2000 
+// word 和 prefix 仅由小写英文字母组成 
+// insert、search 和 startsWith 调用次数 总计 不超过 3 * 10⁴ 次 
+// 
+//
+// Related Topics 设计 字典树 哈希表 字符串 👍 1805 👎 0
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Trie {
+public:
+    struct TrieTree {
+        bool isEnd;
+        TrieTree *next[26];
+
+        TrieTree() {
+            isEnd = false;
+            for (int i = 0; i < 26; i += 1) {
+                next[i] = nullptr;
+            }
+        }
+    } *head;
+
+
+    /** Initialize your data structure here. */
+    Trie() {
+        head = new TrieTree();
+    }
+
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+        TrieTree *tmp = head;
+        for (int i = 0; i < word.size(); i += 1) {
+            if (tmp->next[word[i] - 'a'] == nullptr) {
+                TrieTree *next = new TrieTree();
+                tmp->next[word[i] - 'a'] = next;
+                tmp = next;
+            } else {
+                tmp = tmp->next[word[i] - 'a'];
+            }
+        }
+        tmp->isEnd = true;
+    }
+
+    /** Returns if the word is in the trie. */
+    bool search(string word) {
+        TrieTree *tmp = head;
+        for (int i = 0; i < word.size(); i += 1) {
+            if (tmp->next[word[i] - 'a'] == nullptr) {
+                return false;
+            } else {
+                tmp = tmp->next[word[i] - 'a'];
+            }
+//            cout << "search " << i << endl;
+        }
+        return tmp->isEnd;
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix) {
+        TrieTree *tmp = head;
+        for (int i = 0; i < prefix.size(); i += 1) {
+            if (tmp->next[prefix[i] - 'a'] == nullptr) {
+                return false;
+            } else {
+                tmp = tmp->next[prefix[i] - 'a'];
+            }
+        }
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
+//leetcode submit region end(Prohibit modification and deletion)
